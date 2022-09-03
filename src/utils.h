6c7cd8944647f23d86a20c21ca828aeed1aecf1c
Kartatz <105828205+Kartatz@users.noreply.github.com>
2022-09-03 09:42:27 -0300

    a

---
+++ src/utils.h
@@ -0,0 +1,8 @@
+void write_stderr(const char * format, ...);
+
+int file_exists(const char* filename);
+size_t get_file_size(const char* filename);
+int remove_file(const char* filename);
+int expand_filename(const char* filename, char** fullpath);
+
+#pragma once
